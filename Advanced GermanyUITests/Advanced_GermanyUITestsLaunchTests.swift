5288ba4f6c3a7c5cfc8888b95fdb20d1a88155bd
Danylo Chantsev <danylo.chantsev@soft-fx.lv>
2022-04-25 18:09:40 +0200

    Initial Commit

---
+++ Advanced GermanyUITests/Advanced_GermanyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Advanced_GermanyUITestsLaunchTests.swift
+//  Advanced GermanyUITests
+//
+//  Created by Home on 25.04.2022.
+//
+
+import XCTest
+
+class Advanced_GermanyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
